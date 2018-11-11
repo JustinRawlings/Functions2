d1fa0084865dd95f5711efe46be8ac79356ab56b
Justin Rawlings <44125137+JustinRawlings@users.noreply.github.com>
2018-11-11 16:31:34 -0600

    Functions2
    Second round of functions program

---
+++ Function1.cpp
@@ -0,0 +1,11 @@
+/*
+	Justin Rawlings
+	Function file.
+*/
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+#include "Functions.h"
+
+using namespace std;
---
+++ Functions.h
@@ -0,0 +1,8 @@
+/*
+	Justin Rawlings
+	Header file for Functions program.
+*/
+
+string getTextFromUser(string prompt); //Gets a string of text from user.
+int getNumbersFromUser(string prompt); //Gets a number from user.
+void tellStory(string name, string noun, int number, string element); //Tells user a story based on input.
